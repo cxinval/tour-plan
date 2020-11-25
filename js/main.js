148d541ef3a433809efd364db039d44549c87771
cxinval <cxinval7@mail.ru>
2020-11-25 21:48:58 +0300

    fix: изменил адаптив

--- js/main.js
+++ js/main.js
@@ -13,42 +13,4 @@ var mySwiper = new Swiper(".swiper-container", {
   },
 });
 
-ymaps.ready(init);
 
-function init() {
-  var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [7.575821, 79.803136],
-        zoom: 10,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    ),
-    // Создаем геообъект с типом геометрии "Точка".
-    myGeoObject = new ymaps.GeoObject({
-      // Опции.
-      // Иконка метки будет растягиваться под размер ее содержимого.
-      preset: "islands#blackStretchyIcon",
-      // Метку можно перемещать.
-      draggable: true,
-    });
-    myPieChart = new ymaps.Placemark(
-    );
-  myMap.geoObjects
-    .add(myGeoObject)
-    .add(myPieChart)
-    .add(
-      new ymaps.Placemark(
-        [7.575821, 79.803136],
-        {
-          balloonContent: "цвет <strong>воды пляжа бонди</strong>",
-        },
-        {
-          preset: "islands#icon",
-          iconColor: "#0095b6",
-        }
-      )
-    );
-}
