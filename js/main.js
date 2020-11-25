f66ef24d0dafbb415c98ffced88f893fd366a3e5
cxinval <cxinval7@mail.ru>
2020-11-25 22:06:54 +0300

    create: добавил Yandex API

--- js/main.js
+++ js/main.js
@@ -13,4 +13,31 @@ var mySwiper = new Swiper(".swiper-container", {
   },
 });
 
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map(
+    "map",
+    {
+      center: [41.039671, 28.966327],
+      zoom: 16,
+    },
+    {
+      searchControlProvider: "yandex#search",
+    }
+  );
+  myMap.geoObjects.add(
+    new ymaps.Placemark(
+      [41.039671, 28.966327],
+      {
+        balloonContent: "цвет <strong>воды пляжа бонди</strong>",
+      },
+      {
+        preset: "islands#icon",
+        iconColor: "#0095b6",
+      }
+    )
+  );
+}
+
 
