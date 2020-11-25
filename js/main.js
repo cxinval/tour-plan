190a0e6a220567625629f8ccd92143afb04bd5a4
cxinval <cxinval7@mail.ru>
2020-11-25 17:00:18 +0300

    create: добавил карту и сделал адаптив

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var mySwiper = new Swiper(".swiper-container", {
   // Optional parameters
   loop: true,
-  width: 728, 
+
   // Navigation arrows
   navigation: {
     nextEl: ".slider-button--next",
     prevEl: ".slider-button--prev",
   },
-  keyboard:{
-    enabled:true,
-    onlyInviewport:true,
+  keyboard: {
+    enabled: true,
+    onlyInviewport: true,
   },
 });
 
