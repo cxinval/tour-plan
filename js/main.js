73d0676e850f8aff3733b439e53a752f8c575ab9
cxinval <cxinval7@mail.ru>
2020-11-27 08:48:42 +0300

    create: added reviews section & fixed hotel slider

--- js/main.js
+++ js/main.js
@@ -1,17 +1,36 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   keyboard: {
     enabled: true,
     onlyInviewport: true,
   },
 });
+
+
+
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
+  },
+});
+
+
+
+
+
+
+
 
 ymaps.ready(init);
 
