303fbb214de3c83f3428a437c70371441b2ab9f3
cxinval <cxinval7@mail.ru>
2020-12-10 23:21:24 +0300

    create: сжал файлы

--- js/main.js
+++ js/main.js
@@ -24,32 +24,7 @@ $(document).ready(function () {
     },
   });
 
-  ymaps.ready(init);
-
-  function init() {
-    var myMap = new ymaps.Map(
-      "map",
-      {
-        center: [41.039671, 28.966327],
-        zoom: 16,
-      },
-      {
-        searchControlProvider: "yandex#search",
-      }
-    );
-    myMap.geoObjects.add(
-      new ymaps.Placemark(
-        [41.039671, 28.966327],
-        {
-          balloonContent: "цвет <strong>воды пляжа бонди</strong>",
-        },
-        {
-          preset: "islands#icon",
-          iconColor: "#0095b6",
-        }
-      )
-    );
-  }
+  
 
   $(".menu-button").click(function () {
     $(".navbar__bottom").toggleClass("navbar__bottom-active");
